9e2035d3a2e8e230ade380d5aa40198309970883
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-06-08 16:30:08 -0400

    added semi colons to exercise 2, began exercise 1, can't push to github, at lga

--- main.js
+++ main.js
@@ -13,11 +13,13 @@ console.log(x + " " + y);
 
 // Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console.
 
-var seasonal = ["cotton candy", "guardians mix", "brownie batter"]
+var seasonal = ["cotton candy", "guardians mix", "brownie batter"];
 
-var DQ_blizzards = ["oreo", "butterfinger", "M&M", seasonal]
+var DQ_blizzards = ["oreo", "butterfinger", "M&M", seasonal];
 
 console.log(DQ_blizzards[3][0]);
 console.log(DQ_blizzards[3][2]);
 
 // Write a script that checks if a variable is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
+
+var x = 89;
