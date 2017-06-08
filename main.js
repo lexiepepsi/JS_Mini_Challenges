07dd3734b8d90fa9083068a1206d648b243535b0
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-06-08 01:33:34 -0400

    exercise one complete, working on exercise 2

---
+++ main.js
@@ -0,0 +1,22 @@
+// Create a script with two variables, each assigned to a number. Add them together and output the result to the console. Now do the same with two strings.
+
+var x = 25;
+var y = 38;
+
+console.log(x + y);
+
+
+var x = "hi there";
+var y = "shirmung!";
+
+console.log(x + " " + y);
+
+// Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console.
+
+var seasonal = ["cotton candy", "guardians mix", "brown batter"]
+
+var DQ_blizzards = ["oreo", "butterfinger", "M&M", seasonal]
+
+console.log(DQ_blizzards);
+
+// Write a script that checks if a variable is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
