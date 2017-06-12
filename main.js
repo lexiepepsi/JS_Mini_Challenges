4ce8b49d8c0706ab0629dc5eb94d17f5bc6257de
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-06-11 20:29:58 -0500

    began 3rd exercise

--- main.js
+++ main.js
@@ -23,3 +23,5 @@ console.log(DQ_blizzards[3][2]);
 // Write a script that checks if a variable is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
 
 var x = 89;
+
+if x > 100
